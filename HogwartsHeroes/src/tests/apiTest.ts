8e87d7d91ee4093e57adc0d626b1f665752fe10d
Mateusz Maślanka <91078604+maslankamateusz@users.noreply.github.com>
2024-11-23 20:29:02 +0100

    merge: feature/api into dev

--- HogwartsHeroes/src/tests/apiTest.ts
+++ HogwartsHeroes/src/tests/apiTest.ts
@@ -3,19 +3,7 @@ import { getAllCharacters, searchCharacters, getFilteredCharacters, getCharacter
 
 (async () => {
   try {
-    // console.log('Fetching all characters...');
-    // const charactersList = await getAllCharacters();
-    // console.log(charactersList[1240]);
-
-    // const character = await searchCharacters("Harry potter");
-    // console.log(character);
-
-    // const filteredCharacters = await getFilteredCharacters({ house: 'Gryffindor', gender: 'Male'});
-    // console.log(filteredCharacters);
-    
-    const characterDetail = await getCharacterDetails("d1f94573-614b-4e67-a372-2ff5ef641456");
-    console.log(characterDetail);
-
+  
   } catch (error) {
     console.error('Test failed:', error);
   }
