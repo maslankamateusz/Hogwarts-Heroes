54a3575f0a0135a5ec22df3d10e21a192792ec36
Mateusz Maślanka <91078604+maslankamateusz@users.noreply.github.com>
2024-11-23 19:05:49 +0100

    feat: add character search function

--- HogwartsHeroes/src/api/characterApi.ts
+++ HogwartsHeroes/src/api/characterApi.ts
@@ -14,16 +14,61 @@ export const processCharacters = (rawCharacters: any[]): Character[] => {
   }));
 };
 
+export const getAllCharacters = async (): Promise<Character[]> => {
+  let allCharacters: Character[] = [];
+  let currentPage = 1;
+  let lastPage = 1;
 
-export const getCharacters = async (): Promise<Character[]> => {
   try {
-    const response = await apiClient.get('/characters');
-    const rawCharacters = response.data.data;
-    const processedCharacters = processCharacters(rawCharacters);
+    while (currentPage <= lastPage) {
+      const response = await apiClient.get('/characters', {
+        params: {
+          'page[number]': currentPage,  
+        },
+      });
 
-    return processedCharacters; 
+      const rawCharacters = response.data.data;
+      const processedCharacters = processCharacters(rawCharacters);
+      allCharacters = [...allCharacters, ...processedCharacters];
+
+      lastPage = response.data.meta.pagination.last;
+
+      if (currentPage < lastPage) {
+        currentPage++;
+      } else {
+        break;
+      }
+    }
+
+    return allCharacters;
   } catch (error) {
-    console.error('Error fetching characters:', error);
+    console.error('Error fetching all characters:', error);
+    throw error;
+  }
+};
+
+
+export const searchCharacters = async (searchQuery: string): Promise<Character[]> => {
+  if (!searchQuery.trim()) {
+    return await getAllCharacters(); 
+  }
+
+  let allCharacters: Character[] = [];
+
+
+  try {
+    allCharacters = await getAllCharacters();
+
+    const searchWords = searchQuery.trim().split(/\s+/);
+
+    const filteredCharacters = allCharacters.filter(character => {
+      return searchWords.every(word => character.name.toLowerCase().includes(word.toLowerCase()));
+    });
+
+    return filteredCharacters;
+
+  } catch (error) {
+    console.error('Error fetching characters by search:', error);
     throw error;
   }
 };
--- HogwartsHeroes/src/tests/apiTest.ts
+++ HogwartsHeroes/src/tests/apiTest.ts
@@ -1,11 +1,14 @@
-import { getCharacters } from '../api/characterApi';
+import { getAllCharacters, searchCharacters } from '../api/characterApi';
 
 
 (async () => {
   try {
-    console.log('Fetching all characters...');
-    const charactersList = await getCharacters();
-    console.log(charactersList);
+    // console.log('Fetching all characters...');
+    // const charactersList = await getAllCharacters();
+    // console.log(charactersList[1240]);
+
+    const character = await searchCharacters("Harry potter");
+    console.log(character);
 
   } catch (error) {
     console.error('Test failed:', error);
